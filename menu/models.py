c0cd7d7e2aa96e27c6a5f047b8f89fffde1b84b4
minami <minami3506@outlook.jp>
2026-09-07 17:43:55 +0900

    Allow blank category in Dish model

--- menu/models.py
+++ menu/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Dish(models.Model):
-    name = models.CharField('料理名', max_length=100)
-    last_cooked_date = models.DateField('最後に作った日', null=True, blank=True)
-    memo = models.TextField('メモ・ポイント', blank=True)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, blank=True, default='')  # 空文字を許可
+    last_cooked_date = models.DateField(null=True, blank=True)
+    memo = models.TextField(blank=True, default='')
 
     def __str__(self):
         return self.name
